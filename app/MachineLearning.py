d26cdb39658a3f0b53ded0bfd568ef54b495c8a5
moe18 <chabotmoe1@gmail.com>
2024-03-28 15:26:43 -0400

    added nav bar along with read me page

---
+++ app/MachineLearning.py
@@ -0,0 +1,5 @@
+import streamlit as st
+
+def app():
+    st.title('Machine Learning')
+    st.write('Coming soon')
---
+++ app/about.py
@@ -0,0 +1,27 @@
+import streamlit as st
+
+def app():
+    st.markdown("""# About DeepMLeet
+
+**Welcome to DeepMLeet**, your new go-to platform for diving deep into the world of Machine Learning (ML) and Deep Learning (DL) through challenge-based learning. Inspired by the ethos of platforms like LeetCode, DeepMLeet brings a unique twist, focusing exclusively on the realms of ML and DL. It's more than just a platform; it's a community-driven project where enthusiasts and practitioners come together to test their skills, learn, and grow.
+
+## Our Mission
+
+At DeepMLeet, we believe that the best way to master ML and DL is through practice and collaboration. Our mission is to provide an accessible, engaging, and continuously evolving space where anyone, from beginners to seasoned experts, can come to polish their skills, tackle intriguing problems, and contribute to a body of knowledge that benefits the entire community.
+
+## What We Offer
+
+- **A Growing Collection of Challenges:** From foundational Linear Algebra to cutting-edge Deep Learning techniques, our challenges are designed to cater to learners at all levels. Each problem comes with a detailed description, starter code, and test cases to help you focus on solving and learning.
+- **Interactive Learning Experience:** With our platform, you get immediate feedback on your solutions, thanks to the integration of tools like Streamlit and pistonpy. This real-time execution environment makes learning interactive and fun.
+- **Community Contributions:** DeepMLeet is proudly open-source. We invite our users to contribute by adding new problems, enhancing existing ones, or suggesting features. It's a collaborative effort to make DeepMLeet better for everyone.
+
+## Join the Community
+
+Ready to take on the challenge? Join DeepMLeet today and be part of a growing community of ML/DL enthusiasts dedicated to learning and sharing their knowledge. Whether you're looking to improve your skills, contribute to the platform, or just explore what others have created, there's a place for you here.
+
+## Contribute on GitHub
+
+DeepMLeet is open source, and we welcome contributions of all kinds. Whether you have a suggestion for a new challenge, improvements to our platform, or want to help fix bugs, your input is valuable. Check out our project on GitHub at [moe18/DeepMLeet](https://github.com/moe18/DeepMLeet) and see how you can contribute.
+
+Thank you for being part of our journey. Together, we're not just learning – we're building the future of ML and DL education.
+""")
