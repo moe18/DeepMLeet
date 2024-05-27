4dc9d33e977ab5ee247364ea8e35691c3a684bd1
Muhammad Mujtaba Hussain <64016614+mmujtabah@users.noreply.github.com>
2024-05-27 07:45:49 +0500

    Update

--- app/lin_alg_problems.py
+++ app/lin_alg_problems.py
@@ -183,7 +183,7 @@ The shape of matrix \(M\) is defined by the number of rows and columns it contai
 },
 "Calculate Mean by Row or Column (easy)":{
     "description": "Write a Python function that calculates the mean of a matrix either by row or by column, based on a given mode. The function should take a matrix (list of lists) and a mode ('row' or 'column') as input and return a list of means according to the specified mode.",
-    "example": """Example:
+    "example": """Example1:
         input: matrix = [[1, 2, 3], [4, 5, 6], [7, 8, 9]], mode = 'column'
         output: [4.0, 5.0, 6.0]
         reasoning: Calculating the mean of each column results in [(1+4+7)/3, (2+5+8)/3, (3+6+9)/3].
